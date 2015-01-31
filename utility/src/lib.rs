e467e28c2177c9f2d3ff079e783a19e84a344d0e
Nikita Pekin <contact@nikitapek.in>
2015-01-31 17:40:58 -0500

    Add utility crate

---
+++ utility/src/lib.rs
@@ -0,0 +1,27 @@
+#[derive(Copy)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Copy)]
+pub struct Bounds {
+    pub min: Point,
+    pub max: Point,
+}
+
+impl Bounds {
+    pub fn new(min_x: i32, min_y: i32, max_x: i32, max_y: i32) -> Bounds {
+        Bounds {
+            min: Point { x: min_x, y: min_y },
+            max: Point { x: max_x, y: max_y },
+        }
+    }
+
+    pub fn contains(&self, point: Point) -> bool {
+        point.x >= self.min.x &&
+        point.x < self.max.x &&
+        point.y >= self.min.y &&
+        point.y < self.max.y
+    }
+}

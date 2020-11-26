d90f3b538180a8dc984bc92b4ad7b6cfe6e7e204
Nikita Pekin <nikita@frecency.com>
2020-11-25 21:28:19 -0500

    feat: Add some basic building-blocks meshing with upstream snappy changes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ extern crate bevy_mod_picking;
 #[cfg(target_arch = "wasm32")]
 extern crate bevy_webgl2;
 extern crate building_blocks;
-extern crate nalgebra;
 extern crate rand;
-extern crate tessellation;
 
 mod camera;
 mod greeting;
@@ -13,12 +11,15 @@ mod greeting;
 use std::convert::TryInto;
 
 use bevy::prelude::*;
-use bevy::render::mesh::Indices;
+use bevy::render::mesh::{Indices, VertexAttributeValues};
 use bevy::render::pipeline::PrimitiveTopology;
+use bevy::tasks::{ComputeTaskPool, TaskPool};
 use bevy_mod_picking::*;
-use nalgebra::{Point3, Vector3};
+use building_blocks::core::{Extent3i, PointN};
+use building_blocks::mesh::{MaterialVoxel, PosNormMesh, GreedyQuadsBuffer, greedy_quads, padded_greedy_quads_chunk_extent};
+use building_blocks::storage::{ChunkMap, ChunkMapReader, Array3, IsEmpty, Snappy};
+use building_blocks::prelude::{copy_extent, LocalChunkCache3};
 use rand::distributions::{Distribution, Uniform};
-use tessellation::{BoundingBox, ImplicitFunction, ManifoldDualContouring/*, RealField*/};
 
 use camera::fps::{CameraState, CameraMovementPlugin};
 use greeting::HelloPlugin;
@@ -100,59 +101,6 @@ fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
     }
 }
 
-fn vec3_sub([ax, ay, az]: [f32; 3], [bx, by, bz]: [f32; 3]) -> [f32; 3] {
-    [ax - bx, ay - by, az - bz]
-}
-
-fn vec3_cross([ax, ay, az]: [f32; 3], [bx, by, bz]: [f32; 3]) -> [f32; 3] {
-    [ay * bz - az * by, az * bx - ax * bz, ax * by - ay * bx]
-}
-
-fn vec3_add([ax, ay, az]: [f32; 3], [bx, by, bz]: [f32; 3]) -> [f32; 3] {
-    [ax + bx, ay + by, az + bz]
-}
-
-fn vec3_normalize([x, y, z]: [f32; 3]) -> [f32; 3] {
-    let m = f32::sqrt(x * x  + y * y + z * z); // magnitude
-    [x / m, y / m, z / m]
-}
-
-pub(crate) fn meshify_chunk(_chunk: &Chunk) -> Mesh {
-    let sphere = UnitSphere::new();
-    let mut mdc = ManifoldDualContouring::new(&sphere, 0.2, 0.1);
-    let triangles = mdc.tessellate().unwrap();
-
-    let indices = Indices::U32(triangles.faces.iter().flatten().map(|i| *i as u32).collect::<Vec<u32>>());
-    let mut normals: Vec<[f32; 3]> = vec![[0.0; 3]; triangles.vertices.len()];
-    for i in 0..triangles.faces.len() {
-        let ia = triangles.faces[i][0];
-        let ib = triangles.faces[i][1];
-        let ic = triangles.faces[i][2];
- 
-        let e1 = vec3_sub(triangles.vertex32(ia), triangles.vertex32(ib));
-        let e2 = vec3_sub(triangles.vertex32(ic), triangles.vertex32(ib));
-        let no = vec3_cross(e1, e2);
-
-        normals[ia] = vec3_add(normals[ia], no);
-        normals[ib] = vec3_add(normals[ib], no);
-        normals[ic] = vec3_add(normals[ic], no);
-    }
-    normals.iter_mut().for_each(|n| *n = vec3_normalize(*n));
-    let positions = triangles.vertices;
-    // Generate some zeroed-out (i.e. not correct) UVs to resolve an issue
-    // where the WASM doesn't load on Chrome but works on Firefox.
-    let uvs = vec![[0.; 2]; positions.len()];
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_indices(Some(indices));
-    assert_eq!(positions.len(), normals.len(), "Must have same vertex count ({}) as normal count ({}) in this mesh", positions.len(), normals.len());
-    assert_eq!(positions.len(), uvs.len(), "Must have same vertex count ({}) as uv count ({}) in this mesh", positions.len(), uvs.len());
-    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-    mesh
-}
-
 const CHUNK_LEN: usize = 4096; // 16 * 16 * 16
 
 struct Chunk([i8; CHUNK_LEN]);
@@ -163,30 +111,6 @@ impl Default for Chunk {
         let rng = rand::thread_rng();
         let boxed_array: Box<[i8; CHUNK_LEN]> = terrain_density_range.sample_iter(rng).take(CHUNK_LEN).collect::<Vec<_>>().into_boxed_slice().try_into().expect(&format!("expected a vec of length {}", CHUNK_LEN));
         Self(*boxed_array)
-    }
-}
-
-struct UnitSphere {
-    bbox: BoundingBox<f32>,
-}
-
-impl UnitSphere {
-    fn new() -> Self {
-        Self { bbox: BoundingBox::new(&Point3::new(-10., -10., -10.), &Point3::new(10., 10., 10.)) }
-    }
-}
-
-impl ImplicitFunction<f32> for UnitSphere {
-    fn bbox(&self) -> &BoundingBox<f32> {
-        &self.bbox
-    }
-
-    fn value(&self, p: &Point3<f32>) -> f32 {
-        Vector3::new(p.x, p.y, p.z).norm() - 10.0
-    }
-
-    fn normal(&self, p: &Point3<f32>) -> Vector3<f32> {
-        Vector3::new(p.x, p.y, p.z).normalize()
     }
 }
 
@@ -201,13 +125,6 @@ impl MeshGeneratorState {
 }
 
 const CHUNK_SIZE: i32 = 16;
-
-use bevy::tasks::{ComputeTaskPool, TaskPool};
-use bevy::render::mesh::VertexAttributeValues;
-use building_blocks::core::{Extent3i, PointN};
-use building_blocks::mesh::{MaterialVoxel, PosNormMesh, GreedyQuadsBuffer, greedy_quads, padded_greedy_quads_chunk_extent};
-use building_blocks::prelude::{copy_extent, ChunkMapReader3, LocalChunkCache3, FastSnappy, ChunkMap3};
-use building_blocks::storage::{Array3, IsEmpty};
 
 #[derive(Default)]
 pub struct MeshMaterial(pub Handle<StandardMaterial>);
@@ -275,13 +192,13 @@ fn generate_chunk_meshes_from_cubic(cubic: Cubic, pool: &TaskPool) -> Vec<Option
     let ambient_value = CubeVoxel(false);
     let default_chunk_meta = ();
     // Normally we'd keep this map around in a resource, but we don't need to for this specific
-    // example. We could also use an Array3 here instead of a ChunkMap3, but we use chunks for
+    // example. We could also use an Array3 here instead of a ChunkMap, but we use chunks for
     // educational purposes.
-    let mut map = ChunkMap3::new(
+    let mut map = ChunkMap::new(
         chunk_shape,
         ambient_value,
         default_chunk_meta,
-        FastSnappy,
+        Snappy,
     );
     copy_extent(voxels.extent(), &voxels, &mut map);
 
@@ -292,7 +209,7 @@ fn generate_chunk_meshes_from_cubic(cubic: Cubic, pool: &TaskPool) -> Vec<Option
         for chunk_key in map_ref.chunk_keys() {
             s.spawn(async move {
                 let local_cache = LocalChunkCache3::new();
-                let map_reader = ChunkMapReader3::new(map_ref, &local_cache);
+                let map_reader = ChunkMapReader::new(map_ref, &local_cache);
                 let padded_chunk_extent =
                     padded_greedy_quads_chunk_extent(&map_ref.extent_for_chunk_at_key(chunk_key));
 
